aeb920e737da3edfa655c35ad1ab9f77a683eccf
Alexander Korn <alexander@korn-online.org>
2019-04-09 23:22:41 +0200

    Testing with dithering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate image;
+
+use image::FilterType;
+use image::imageops::colorops::{BiLevel, dither};
+
 fn main() {
     println!("Hello, world!");
+
+    conv_img();
+}
+
+fn conv_img() {
+    let mut img = image::open("test.png").unwrap();
+    img = img.resize(100, 100, FilterType::Nearest);
+    img = img.grayscale();
+
+    let mut img_buf = img.as_mut_luma8().unwrap();
+    dither(&mut img_buf, &BiLevel);
+
+    img_buf.save("test2.png").unwrap();
 }
